e3b1bca5d6a055e3ee9124dd67b0a3be995ed23c
eztwokey <eztwokey@bk.ru>
2020-11-23 17:44:37 +0300

    povish.py

--- povish.py
+++ povish.py
@@ -1,2 +1,3 @@
-(lambda hour, minute, second : print(abs(hour * 30 + minute * 0.5 + second * 0.5 / 60))) \
+(lambda hour, minute, second:
+ print(abs(hour * 30 + minute * 0.5 + second * 0.5/60))) \
 (float(input()), float(input()), float(input()))
